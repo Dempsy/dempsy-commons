016b34cbe4c69303af97c3b7f4611219f74b08c0
Jim Carroll <jim@dontcallme.com>
2019-01-16 17:17:08 -0500

    Added a delayed creation of the AutoCloseable for the CloseableRule.

--- dempsy-test-utils/src/main/java/net/dempsy/utils/test/CloseableRule.java
+++ dempsy-test-utils/src/main/java/net/dempsy/utils/test/CloseableRule.java
@@ -5,11 +5,13 @@ import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 
 import org.junit.rules.ExternalResource;
 
 /**
- * A {@link CloseableRule} can be used to manage resources for unit tests that are {@link AutoCloseable}s.
+ * A {@link CloseableRule} can be used to manage resources for unit tests that
+ * are {@link AutoCloseable}s.
  * 
  * <pre>
  * {@code
@@ -19,54 +21,81 @@ import org.junit.rules.ExternalResource;
 </pre>
  */
 public class CloseableRule extends ExternalResource {
-   private final AutoCloseable[] autoCloseables;
+    private final AutoCloseable[] autoCloseables;
+    private final Supplier<AutoCloseable> supplier;
 
-   private final String newLine = System.getProperty("line.separator");
+    private final String newLine = System.getProperty("line.separator");
 
-   public CloseableRule(final AutoCloseable... autoCloseables) {
-      this.autoCloseables = autoCloseables;
-   }
+    /**
+     * Create a JUnit rule that automatically closes the {@link AutoCloseable}s
+     * passed in.
+     * 
+     * @param autoCloseables - {@link AutoCloseable}s to close when at the
+     *            conclusion of the test scope.
+     */
+    public CloseableRule(final AutoCloseable... autoCloseables) {
+        this.autoCloseables = autoCloseables;
+        this.supplier = null;
+    }
 
-   @SuppressWarnings("unchecked")
-   public <T extends AutoCloseable> T get(final Class<T> clazz) {
-      return (T)Arrays.stream(autoCloseables)
+    /**
+     * Create a JUnit rule that creates an {@link AutoCloseable} through the given
+     * {@link Supplier} during the setup phase of the test and closes the
+     * {@link AutoCloseable} during the tear-down phase of the test.
+     * 
+     * @param supplier - the source of the given {@link AutoCloseable}
+     */
+    public CloseableRule(final Supplier<AutoCloseable> supplier) {
+        this.autoCloseables = new AutoCloseable[1];
+        this.supplier = supplier;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends AutoCloseable> T get(final Class<T> clazz) {
+        return (T)Arrays.stream(autoCloseables)
             .filter(o -> clazz.isAssignableFrom(o.getClass()))
             .findFirst()
             .orElse(null);
-   }
+    }
 
-   /**
-    * On the {@code after} test phase, close all of the {@link AutoCloseable}'s that were passed in the constructor.
-    */
-   @Override
-   protected void after() {
-      final List<Exception> exceptions = new ArrayList<>();
-      Arrays.stream(autoCloseables)
+    protected void before() {
+        if(supplier != null)
+            autoCloseables[0] = supplier.get();
+    }
+
+    /**
+     * On the {@code after} test phase, close all of the {@link AutoCloseable}'s
+     * that were passed in the constructor.
+     */
+    @Override
+    protected void after() {
+        final List<Exception> exceptions = new ArrayList<>();
+        Arrays.stream(autoCloseables)
             .filter(ac -> ac != null)
             .forEach(ac -> {
-               try {
-                  ac.close();
-               } catch(final Exception e) {
-                  exceptions.add(e);
-               }
+                try {
+                    ac.close();
+                } catch(final Exception e) {
+                    exceptions.add(e);
+                }
             });
-      if(exceptions.size() > 0) {
-         throw new Error("Exceptions thrown while closing:" + newLine + formatExceptionListAsMessage(exceptions), exceptions.get(0));
-      }
-   }
+        if(exceptions.size() > 0) {
+            throw new Error("Exceptions thrown while closing:" + newLine + formatExceptionListAsMessage(exceptions), exceptions.get(0));
+        }
+    }
 
-   private String formatExceptionListAsMessage(final List<Exception> exceptions) {
-      final StringBuilder sb = new StringBuilder();
-      exceptions.stream()
+    private String formatExceptionListAsMessage(final List<Exception> exceptions) {
+        final StringBuilder sb = new StringBuilder();
+        exceptions.stream()
             .forEach(e -> {
-               final StringWriter stringWriter = new StringWriter();
-               try (final PrintWriter w = new PrintWriter(stringWriter)) {
-                  sb.append("Exception thrown during close: ");
-                  w.println();
-                  e.printStackTrace(w);
-               }
-               sb.append(stringWriter.toString());
+                final StringWriter stringWriter = new StringWriter();
+                try (final PrintWriter w = new PrintWriter(stringWriter)) {
+                    sb.append("Exception thrown during close: ");
+                    w.println();
+                    e.printStackTrace(w);
+                }
+                sb.append(stringWriter.toString());
             });
-      return sb.toString();
-   }
+        return sb.toString();
+    }
 }
