c37e199dabd04bd357fbb41e099c93823069e3ac
Jim Carroll <jim@dontcallme.com>
2019-09-20 15:32:01 -0400

    Fix a bug in the ClasspathPropertiesReader.

--- dempsy-distconfig.classpath/src/main/java/net/dempsy/distconfig/classpath/ClasspathPropertiesReader.java
+++ dempsy-distconfig.classpath/src/main/java/net/dempsy/distconfig/classpath/ClasspathPropertiesReader.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,16 +22,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.dempsy.distconfig.PropertiesReader;
 import net.dempsy.distconfig.PropertiesWatcher;
 import net.dempsy.util.SafeString;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class ClasspathPropertiesReader implements PropertiesReader {
     private static final Logger LOGGER = LoggerFactory.getLogger(ClasspathPropertiesReader.class);
-    
+
     private final boolean actLikeYouSupportNotifications;
     private final String classpathUri;
 
@@ -46,25 +46,26 @@ public class ClasspathPropertiesReader implements PropertiesReader {
 
     private static String cleanPath(final String path) {
         String trimmed = path.trim();
-        if (trimmed.startsWith("/"))
+        if(trimmed.startsWith("/"))
             trimmed = cleanPath(trimmed.substring(1, trimmed.length()));
-        if (trimmed.endsWith("/"))
+        if(trimmed.endsWith("/"))
             trimmed = trimmed.substring(0, trimmed.length() - 1); // chop off trailing '/'. Let's hope there's not more than one.
         return trimmed;
     }
 
     @Override
     public VersionedProperties read(final PropertiesWatcher watcher) throws IOException {
-        if (watcher != null && !actLikeYouSupportNotifications)
+        if(watcher != null && !actLikeYouSupportNotifications)
             throw new UnsupportedOperationException(
-                    "Cannot add watch \"" + SafeString.objectDescription(watcher) + " to a " + ClasspathPropertiesReader.class.getSimpleName());
+                "Cannot add watch \"" + SafeString.objectDescription(watcher) + " to a " + ClasspathPropertiesReader.class.getSimpleName());
 
-        final String resource = (classpathUri.startsWith("classpath:///")) ? classpathUri.substring(14)
-                : (classpathUri.startsWith("classpathUri:") ? classpathUri.substring(11) : cleanPath(classpathUri));
+        final String resource = (classpathUri.startsWith("classpath:///")) ? classpathUri.substring(13)
+            : (classpathUri.startsWith("classpath:") ? classpathUri.substring(10) : cleanPath(classpathUri));
 
         LOGGER.debug("trying to read:{}", resource);
         final InputStream resAsStream = ClasspathPropertiesReader.class.getClassLoader().getResourceAsStream(resource);
-        return resAsStream == null ? new VersionedProperties(-1, null) : new VersionedProperties(0,
+        return resAsStream == null ? new VersionedProperties(-1, null)
+            : new VersionedProperties(0,
                 chainThrows(new Properties(), p -> p.load(resAsStream)));
     }
 
